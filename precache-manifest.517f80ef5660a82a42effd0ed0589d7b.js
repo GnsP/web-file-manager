2be720711f2efd5eaf7548784bfba83c8c42448d
gnsp <sir.gnsp@gmail.com>
2019-12-27 14:43:59 +0530

    add build files in gh-pages

---
+++ precache-manifest.517f80ef5660a82a42effd0ed0589d7b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50bc4e026dcca90b1400a0c6c0edee88",
+    "url": "/web-file-manager/index.html"
+  },
+  {
+    "revision": "937428d0017b390e278d",
+    "url": "/web-file-manager/static/css/main.dba6ae67.chunk.css"
+  },
+  {
+    "revision": "0ff1a58dc3cc697677f8",
+    "url": "/web-file-manager/static/js/2.df24221c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/web-file-manager/static/js/2.df24221c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "937428d0017b390e278d",
+    "url": "/web-file-manager/static/js/main.7c412476.chunk.js"
+  },
+  {
+    "revision": "65845439c403b0c36aed",
+    "url": "/web-file-manager/static/js/runtime-main.316ee2e7.js"
+  },
+  {
+    "revision": "f6486474118e37ee108ce4eb46acba6c",
+    "url": "/web-file-manager/static/media/close.f6486474.svg"
+  },
+  {
+    "revision": "a048334606230b886006a4191115ca27",
+    "url": "/web-file-manager/static/media/dropdown.a0483346.svg"
+  },
+  {
+    "revision": "f47dc9acf919fafb0e2c198ef78d139f",
+    "url": "/web-file-manager/static/media/search.f47dc9ac.svg"
+  }
+]);
